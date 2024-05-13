3152af31aa2a32c2733cd6e38b46d8e16d14b587
Drew Karriker <drewlearns@hey.com>
2024-05-13 15:51:19 -0400

    + getIncomes lambda

--- lambdas.tf
+++ lambdas.tf
@@ -94,7 +94,7 @@ module "lambdas" {
       runtime       = "nodejs20.x"
       method        = "POST"               # CAN ONLY BE POST
       authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
-      policy_arns   = [aws_iam_policy.forgot_password_policy.arn]
+      policy_arns   = []
       environment = {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
 
@@ -104,7 +104,7 @@ module "lambdas" {
       runtime       = "nodejs20.x"
       method        = "POST"               # CAN ONLY BE POST
       authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
-      policy_arns   = [aws_iam_policy.forgot_password_policy.arn]
+      policy_arns   = []
       environment = {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
       }
@@ -113,7 +113,7 @@ module "lambdas" {
       runtime       = "nodejs20.x"
       method        = "POST"               # CAN ONLY BE POST
       authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
-      policy_arns   = [aws_iam_policy.forgot_password_policy.arn]
+      policy_arns   = []
       environment = {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
       }
@@ -122,7 +122,16 @@ module "lambdas" {
       runtime       = "nodejs20.x"
       method        = "POST"               # CAN ONLY BE POST
       authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
-      policy_arns   = [aws_iam_policy.forgot_password_policy.arn]
+      policy_arns   = []
+      environment = {
+        DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
+      }
+    }
+    "getIncomes" = {
+      runtime       = "nodejs20.x"
+      method        = "POST"               # CAN ONLY BE POST
+      authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
+      policy_arns   = []
       environment = {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
       }
