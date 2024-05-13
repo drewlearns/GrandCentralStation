464178ab27015f2c57dfec686ae31c16cead3827
Drew Karriker <drewlearns@hey.com>
2024-05-13 15:44:00 -0400

    + editFamily

--- lambdas.tf
+++ lambdas.tf
@@ -118,6 +118,15 @@ module "lambdas" {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
       }
     }
+    "editFamily" = {
+      runtime       = "nodejs20.x"
+      method        = "POST"               # CAN ONLY BE POST
+      authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
+      policy_arns   = [aws_iam_policy.forgot_password_policy.arn]
+      environment = {
+        DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
+      }
+    }
   }
 }
 
