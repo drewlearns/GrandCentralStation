89895e827819c1ba0a37366231c820d7ec15c20c
Drew Karriker <drewlearns@hey.com>
2024-05-14 22:06:19 -0400

    getFamilyTransactionsThisMonth

--- lambdas.tf
+++ lambdas.tf
@@ -149,13 +149,21 @@ module "lambdas" {
     "addTransactionToLedger" = {
       runtime       = "nodejs20.x"
       method        = "POST"               # CAN ONLY BE POST
-      authorization = "NONE" # "NONE" OR "COGNITO_USER_POOLS"
+      authorization = "COGNITO_USER_POOLS" # "NONE" OR "COGNITO_USER_POOLS"
       policy_arns   = [aws_iam_policy.lambda_s3_policy.arn]
       environment = {
         DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
         BUCKET       = "tppb-receipts-${data.aws_caller_identity.current.account_id}"
       }
     }
+    "getFamilyTransactionsThisMonth" = {
+      runtime       = "nodejs20.x"
+      method        = "POST"               # CAN ONLY BE POST
+      authorization = "NONE" # "NONE" OR "COGNITO_USER_POOLS"
+      policy_arns   = [aws_iam_policy.lambda_s3_policy.arn]
+      environment = {
+        DATABASE_URL = "postgresql://root:${aws_secretsmanager_secret_version.db_master_password_version.secret_string}@${aws_rds_cluster_instance.aurora_instance.endpoint}:5432/tppb${var.environment}?schema=public"
+      }
+    }
   }
 }
-
